c5e20cacde0ac7e43ec3d205a09ba7863d30455d
Mark Surnin <marksurnin@gmail.com>
2016-01-09 12:45:03 +0000

    Implemented a basic Note application

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,58 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
+  render () {
+    if (this.state.editing) {
+      return this.renderEdit();
+    }
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type='text'
+      autoFocus={true}
+      placeholder={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+  renderNote = () => {
+    const onDelete = this.props.onDelete;
+    return (
+      <div onClick={this.edit}>
+        <span className='task'>{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null}
+      </div>
+    ); //?
+  };
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    if (e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+  finishEdit = (e) => {
+    if (this.props.onEdit) {
+      this.props.onEdit(e.target.value); // ?
+    }
+    this.setState({
+      editing: false
+    });
+  };
+  renderDelete = () => {
+    return <button
+      className='delete-note'
+      onClick={this.props.onDelete}>x</button>;
+  };
+}
+
+// export default ({task}) => <div>{task}</div>;
